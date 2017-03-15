71a606f48a6d29e17e77d8bec7ec99a4d6fc9576
Canio Massimo Tristano <massimo.tristano@gmail.com>
2017-03-15 23:23:43 +0000

    updated premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -2,6 +2,13 @@ solution "koji"
 	language "C"
 	configurations { "Debug", "Release" }
 	platforms { "x32", "x64" }
+	location "build"
+
+	configuration "Debug"
+		symbols "On"
+	
+	configuration "Release"
+		optimize "Speed"
 
 	project "kojic"
 		kind "ConsoleApp"
